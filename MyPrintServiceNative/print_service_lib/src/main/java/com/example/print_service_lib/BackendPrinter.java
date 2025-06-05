35c9e7ea7836eaaf278da31936485861de9ea70b
SimeranDonnePXL <Simeran.Donne@student.pxl.be>
2025-06-05 16:20:21 +0200

    proof of concepts

--- MyPrintServiceNative/print_service_lib/src/main/java/com/example/print_service_lib/BackendPrinter.java
+++
@@ -1,9 +0,0 @@
-package com.example.print_service_lib;
-
-public class BackendPrinter {
-    private String id;
-    private String name;
-
-    public String getId()   { return id; }
-    public String getName() { return name; }
-}
